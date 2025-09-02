2065ae91b7fa43195fe9c276ef1d404284559b74
Mo7amedDev <medhasnaoui833@gmail.com>
2025-09-02 12:54:08 +0100

    first commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import PinterestProvider from "next-auth/providers/pinterest";
+
+const handler = NextAuth({
+  providers: [
+    PinterestProvider({
+      clientId: process.env.PINTEREST_APP_ID!,
+      clientSecret: process.env.PINTEREST_APP_SECRET!,
+      authorization: {
+        params: {
+          scope: "boards:read,pins:read,pins:write",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
